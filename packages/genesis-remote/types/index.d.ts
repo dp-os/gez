1a3127c7d0641388acf8a160a8d4d744f0b78f3f
lzxb <1340641314@qq.com>
2020-04-27 18:04:30 +0800

    feat: 重写genesis-remote 组件

--- packages/genesis-remote/types/index.d.ts
+++ packages/genesis-remote/types/index.d.ts
@@ -1,4 +1,36 @@
 import Vue from 'vue';
-export default class RemoteView {
-    static install<T>(_Vue: typeof Vue, options?: T): void;
+import { ClientOptions } from '@fmfe/genesis-core';
+export interface RemoteViewData {
+    automount: boolean;
+    html: string;
+    id: string;
+    name: string;
+    style: string;
+    script: string;
+    url: string;
+    state: {
+        [x: string]: any;
+    };
+}
+/**
+ * 加载样式文件
+ */
+export declare const loadStyle: (html: string) => Promise<boolean[]>;
+/**
+ * 加载js文件
+ */
+export declare const loadScript: (html: string) => Promise<boolean[]>;
+/**
+ * 远程调用组件
+ */
+export declare const RemoteView: any;
+declare const _default: {
+    install(_Vue: import("vue").VueConstructor<Vue>): void;
+};
+export default _default;
+declare module 'vue/types/options' {
+    interface ComponentOptions<V extends Vue> {
+        renderContext?: RenderContext;
+        clientOptions?: ClientOptions;
+    }
 }
