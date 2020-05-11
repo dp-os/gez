100e0e18a2579a67f9f300d0f6f701c5312c32e1
lzxb <1340641314@qq.com>
2020-05-11 09:16:19 +0800

    chore: 调整模板

--- packages/genesis-compiler/templace/src/entry-client.ts
+++ packages/genesis-compiler/templace/src/entry-client.ts
@@ -2,8 +2,9 @@ import { ClientOptions } from '@fmfe/genesis-core';
 import Vue from 'vue';
 import App from './app.vue';
 
-export default async (context: ClientOptions): Promise<Vue> => {
+export default async (clientOptions: ClientOptions): Promise<Vue> => {
     return new Vue({
+        clientOptions,
         render(h) {
             return h(App);
         }
