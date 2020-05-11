2a07e49b49f81ba3868d1bf4caf985e9c54b4002
lzxb <1340641314@qq.com>
2020-05-11 09:16:19 +0800

    chore: 调整模板

--- packages/genesis-compiler/templace/src/entry-server.ts
+++ packages/genesis-compiler/templace/src/entry-server.ts
@@ -2,8 +2,9 @@ import { RenderContext } from '@fmfe/genesis-core';
 import Vue from 'vue';
 import App from './app.vue';
 
-export default async (context: RenderContext): Promise<Vue> => {
+export default async (renderContext: RenderContext): Promise<Vue> => {
     return new Vue({
+        renderContext,
         render(h) {
             return h(App);
         }
