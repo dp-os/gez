feb5dcfad7d1d22ff478d4c9cc49dea9b83cc2c9
Anish <anishgeorge2690@gmail.com>
2020-08-01 14:41:20 +0530

    update playground

--- examples/ssr-layout/src/entry-server.ts
+++ examples/ssr-layout/src/entry-server.ts
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import { createServerApp } from '@fmfe/genesis-app';
 import App from './app.vue';
 import { createRouter } from './routes';
+import './main.css';
 
 export default async (renderContext: RenderContext): Promise<Vue> => {
     return createServerApp({
