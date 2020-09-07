cd05478282459722ea828c83dfc1af63bf2e95bc
lzxb <1340641314@qq.com>
2020-09-07 23:17:18 +0800

    build: code

--- packages/genesis-core/dist/ssr-genesis/src/webpack-public-path-client.ts
+++
@@ -1,7 +0,0 @@
-const baseUrl = decodeURIComponent(
-    (window as any)[`__webpack_public_path_${process.env.GENESIS_NAME}__`]
-);
-//
-/* eslint-disable no-undef */
-// @ts-ignore
-__webpack_public_path__ = baseUrl;
