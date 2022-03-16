1b0e19ec970f70ec714ae6648d4521a886db9fab
lzxb <1340641314@qq.com>
2022-03-16 22:16:51 +0800

    feat: try-ts-node

--- packages/genesis-core/bin/try-ts-node.js
+++ packages/genesis-core/bin/try-ts-node.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
-try {
-    require('@fmfe/genesis-compiler/bin/ts-node');
-} catch (e) {}
+if (!__dirname.includes('node_modules')) {
+    try {
+        require('@fmfe/genesis-compiler/bin/ts-node');
+    } catch (e) {}
+}
