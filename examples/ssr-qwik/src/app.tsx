4235008a6a568703d5fa42b5580a0f4b1344fcd0
lzxb <1340641314@qq.com>
2023-11-22 21:23:28 +0800

    feat: 优化qwik例子

--- examples/ssr-qwik/src/app.tsx
+++ examples/ssr-qwik/src/app.tsx
@@ -3,54 +3,58 @@ import {
   component$,
   useStore,
   useVisibleTask$,
-  useContextProvider
+  useContextProvider,
+  useTask$
 } from '@builder.io/qwik'
-import { Count, PROVIDE_STORE_KEY, useState } from './store'
-
-const Child = component$(() => {
-  const count = Count.use()
-  return (
-    <div>
-      4444:{count.value}
-    </div>
-  )
-})
-
-const Add = component$(() => {
-  const state = useState()
-  return (
-    <button
-      onClick$={async () => {
-        Count.use(state).$inc()
-      }}
-    >
-      {Count.use(state).value}
-      +
-    </button>
-  )
-})
+import { Count, PROVIDE_STORE_KEY } from './store'
+import { Child } from './child'
 
 const App = component$(() => {
   const state = createState({
     proxy: useStore
   })
 
+  useContextProvider(PROVIDE_STORE_KEY, state)
+
+  // 客户端需要激活状态
   useVisibleTask$(async () => {
     createState({
       state
     })
   })
-  useContextProvider(PROVIDE_STORE_KEY, state)
+
   const count = Count.use()
+
+  // 模拟服务端请求
+  useTask$(async () => {
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        Count.use(state).$setTime()
+        resolve()
+      }, 200)
+    })
+  })
+
+  // 客户端继续更新状态
+  useVisibleTask$((taskContext) => {
+    const timer = setInterval(Count.use(state).$setTime, 1000)
+
+    taskContext.cleanup(() => {
+      clearInterval(timer)
+    })
+  })
+
   return (
     <>
       <head>
         <title>Title</title>
       </head>
       <body>
-        <div class="count">3333:{count.value}</div>
-        <Child />
-        <Add />
+        <div class="app">
+          <p>Current Time: {count.serverTime}</p>
+          <Child />
+          <p>Click Count: {count.value}</p>
+        </div>
       </body>
     </>
   )
