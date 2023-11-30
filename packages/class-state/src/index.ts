eb00ed17a6ef0ad10af76217a035245470e6285e
lzxb <1340641314@qq.com>
2023-11-30 10:21:47 +0800

    v0.1.19

--- packages/class-state/src/index.ts
+++ packages/class-state/src/index.ts
@@ -1,2 +1,2 @@
 export { createState, type State } from './create'
-export { connectState, connectStore, type StoreConstructor, type StoreContext, type StoreInstance, type StoreParams } from './connect'
+export { connectState, connectStore, type StoreConstructor, type StoreContext, type StoreInstance, type StoreParams, LIFE_CYCLE_CREATED, LIFE_CYCLE_DISPOSE } from './connect'
