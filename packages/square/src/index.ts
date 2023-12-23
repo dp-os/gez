f0d2e74ab7e26a186cab943fd91aeda19c0419ab
lzxb <1340641314@qq.com>
2023-12-23 11:49:17 +0800

    v2.7.16

--- packages/square/src/index.ts
+++ packages/square/src/index.ts
@@ -5,6 +5,6 @@ import { fixMod } from './mod';
 import { Square } from './square';
 import type { Types as MicroTypes } from './types';
 
-const Tms: _Tms = fixMod(_Tms);
+const Tms: typeof _Tms = fixMod(_Tms);
 
 export { Tms, type TmsDepNotifyParams, Micro, type MicroTypes, Square };
