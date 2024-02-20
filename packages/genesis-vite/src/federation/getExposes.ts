06ca867f70bf8117e9c7f06ce31eddeda413e3b5
shizhongyuan1994 <849068437@qq.com>
2024-02-20 18:13:10 +0800

    feat: 下载 dts 到本地node_nodules 目录

--- packages/genesis-vite/src/federation/getExposes.ts
+++ packages/genesis-vite/src/federation/getExposes.ts
@@ -1,4 +1,4 @@
-import { type Genesis } from 'genesis3'
+import { type Genesis, type Remote } from 'genesis3'
 import fs from 'node:fs'
 import path from 'node:path'
 import axios from 'axios'
@@ -6,6 +6,9 @@ import JSZIP, { type JSZipObject } from 'jszip'
 
 import { type Manifest } from '../build/build-federation'
 
+/**
+ * 获取远程依赖
+*/
 export async function getExposes (genesis: Genesis) {
   try {
     const remotes = genesis.federation?.remotes
@@ -14,19 +17,10 @@ export async function getExposes (genesis: Genesis) {
 
       for (const remote of remotes) {
         const { name, clientOrigin } = remote
-        const res = await axios.get<Manifest>(`${clientOrigin}/${name}/node-exposes/manifest.json`)
-        const {
-          // client,
-          server,
-          dts,
-          buildTime
-        } = res.data
-        const url = `${clientOrigin}/${name}/node-exposes/${server}.zip`
-        await loadFile({
-          name,
-          url,
-          dts,
-          buildTime
+        const { data: manifest } = await axios.get<Manifest>(`${clientOrigin}/${name}/node-exposes/manifest.json`)
+        await loadExpose({
+          remote,
+          manifest
         })
       }
     }
@@ -37,27 +31,38 @@ export async function getExposes (genesis: Genesis) {
 }
 
 /**
+ * 根据配置加载远程资源
+ * 如果存在 dts 则一并下载
+*/
+async function loadExpose ({
+  remote,
+  manifest
+}: {
+  remote: Remote
+  manifest: Manifest
+}) {
+  const { name, clientOrigin } = remote
+  const { server, dts } = manifest
+
+  const url = `${clientOrigin}/${name}/node-exposes/${server}.zip`
+  await loadFile(url, `./node_modules/${name}`)
+
+  if (dts) {
+    const url = `${clientOrigin}/${name}/node-exposes/${server}-dts.zip`
+    await loadFile(url, `./node_modules/${name}`)
+  }
+}
+
+/**
  * 加载远程资源并下载到本地
 */
-async function loadFile ({
-  name,
-  url,
-  dts,
-  buildTime
-}: {
-  name: string
-  url: string
-  dts: boolean
-  buildTime: number
-}) {
-  console.log('@loadFile', name, url, dts, buildTime)
+async function loadFile (url: string, savePath: string) {
   const res = await axios.get(url, {
     responseType: 'arraybuffer' // 类型必须为arraybuffer
   })
   const zip = new JSZIP()
   const zipData = await zip.loadAsync(res.data)
-  console.log('@zipData')
-  await saveZipFiles(zipData.files, `./node_modules/${name}`)
+  await saveZipFiles(zipData.files, savePath)
 }
 
 /**
@@ -67,16 +72,15 @@ async function saveZipFiles (files: Record<string, JSZipObject>, savePath: strin
   console.log('@saveZipFiles', savePath)
   for (const filename of Object.keys(files)) {
     const dest = path.join(savePath, filename)
-    console.log('@dest', dest)
     // 如果该文件为目录需先创建文件夹
     if (files[filename].dir) {
-      console.log('@make dir', dest)
+      console.log('@make dir =======', dest)
       fs.mkdirSync(dest, {
         recursive: true
       })
     } else {
       // 把每个文件buffer写到硬盘中
-      console.log('@write file', dest)
+      console.log('@write file =======', dest)
       const content = await files[filename].async('nodebuffer')
       fs.writeFileSync(dest, content)
     }
