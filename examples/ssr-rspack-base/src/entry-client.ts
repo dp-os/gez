60c7cb87171697e1cff1e2032413e66c8ea8d1e1
shizhongyuan1994 <849068437@qq.com>
2024-08-05 17:13:38 +0800

    feat: update

---
+++ examples/ssr-rspack-base/src/entry-client.ts
@@ -0,0 +1,3 @@
+import { createApp } from './main';
+
+createApp();
---
+++ examples/ssr-rspack-base/src/entry-server.ts
@@ -0,0 +1,8 @@
+import { defineServer } from '@gez/core'
+import { createApp } from './main'
+
+export default defineServer({
+  async render (context) {
+    context.html = createApp.toString();
+  }
+})
