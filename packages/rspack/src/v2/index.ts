ce6ba6b01fcf12ded9262892b6e9a7fbf82d874d
lzxb <1340641314@qq.com>
2024-10-11 18:00:53 +0800

    feat: 完成 importmap 构建

--- packages/rspack/src/v2/index.ts
+++ packages/rspack/src/v2/index.ts
@@ -3,15 +3,18 @@ import {
     type Assets,
     type Compilation,
     type Compiler,
-    type EntryStaticNormalized,
-    type Externals,
     type RspackPluginInstance,
-    // type ResolveAlias,
     rspack
 } from '@rspack/core';
 
 export interface ImportmapData {
+    /**
+     * 当前编译的版本号
+     */
     version: string;
+    /**
+     * 映射文件的路径
+     */
     importmapFilePath: string;
     imports: Record<string, string>;
     exposes: Record<string, string>;
@@ -24,14 +27,16 @@ export class ImportmapPlugin implements RspackPluginInstance {
     public options: ParsedModuleConfig;
 
     public constructor(options: ParsedModuleConfig) {
+        console.log('>>>>>>>', options);
         this.options = options;
     }
     public apply(compiler: Compiler) {
-        this.applyExports(compiler);
+        this.applyEntry(compiler);
         this.applyExternals(compiler);
+        this.applyAssets(compiler);
     }
 
-    public applyExports(compiler: Compiler) {
+    public applyEntry(compiler: Compiler) {
         const _entry = compiler.options.entry;
         const wrapEntry =
             typeof _entry === 'function' ? _entry : async () => _entry;
@@ -39,9 +44,9 @@ export class ImportmapPlugin implements RspackPluginInstance {
         compiler.options.entry = async () => {
             const result = await wrapEntry();
             Object.entries(options).forEach(([key, value]) => {
-                const name = key;
-                result[name] = {
+                result[key] = {
                     import: [value],
+                    asyncChunks: false,
                     library: {
                         type: 'module'
                     }
@@ -72,4 +77,81 @@ export class ImportmapPlugin implements RspackPluginInstance {
         compiler.options.externalsType = 'module-import';
         compiler.options.externals = externals;
     }
+    public applyAssets(compiler: Compiler) {
+        compiler.hooks.thisCompilation.tap(
+            'importmap-plugin',
+            (compilation: Compilation) => {
+                compilation.hooks.processAssets.tap(
+                    {
+                        name: 'importmap-plugin',
+                        stage: rspack.Compilation
+                            .PROCESS_ASSETS_STAGE_ADDITIONAL
+                    },
+                    (assets: Assets) => {
+                        const stats = compilation.getStats().toJson({
+                            all: false,
+                            hash: true,
+                            entrypoints: true
+                        });
+                        const entrypoints = stats.entrypoints || {};
+                        const entryMap: Record<string, string> = {};
+                        const imports: Record<string, string> = Object.entries(
+                            entrypoints
+                        ).reduce((acc, [key, value]) => {
+                            console.log('@item.assets', key, value.assets);
+                            const item = value.assets?.[0];
+                            if (item) {
+                                key = key.replace(/^\.\//, '');
+                                const name = item.name.replace(/^\.\//, '');
+                                acc[`${stats.name}/${key}`] =
+                                    `/${stats.name}/${name}`;
+
+                                entryMap[key] = `./${name}`;
+                            }
+                            return acc;
+                        }, {});
+
+                        const { RawSource } = compiler.webpack.sources;
+                        const code = toImportmapJsCode(imports);
+                        compilation.emitAsset(
+                            'importmap.js',
+                            new RawSource(code)
+                        );
+                        compilation.emitAsset(
+                            `importmap.${stats.hash}.js`,
+                            new RawSource(code)
+                        );
+
+                        compilation.emitAsset(
+                            'package.json',
+                            new RawSource(
+                                JSON.stringify(
+                                    {
+                                        name: this.options.name,
+                                        version: '1.0.0',
+                                        type: 'module',
+                                        exports: entryMap
+                                    },
+                                    null,
+                                    4
+                                )
+                            )
+                        );
+                    }
+                );
+            }
+        );
+    }
+}
+
+function toImportmapJsCode(imports: Record<string, string>) {
+    return `
+    ((global) => {
+        const importsMap = ${JSON.stringify(imports)};
+        const importmapKey = '__importmap__';
+        const importmap = global[importmapKey] = global[importmapKey] || {};
+        const imports = importmap.imports = importmap.imports || {};
+        Object.assign(imports, importsMap);
+    })(globalThis);
+    `;
 }
