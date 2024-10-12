e9863b5e98bc9ea314b0e46999d5517c574b0c21
shizhongyuan1994 <849068437@qq.com>
2024-10-12 09:40:07 +0800

    feat: update

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 import fs from 'node:fs';
+import path from 'node:path';
 
 fs.symlinkSync(
-    '/Volumes/work/github/gez/examples/ssr-rspack-vue2_remote/dist/server',
-    '/Volumes/work/github/gez/examples/ssr-rspack-vue2_remote/node_modules/ssr-rspack-vue2_remote'
+    path.resolve(
+        import.meta.dirname,
+        './examples/ssr-rspack-vue2_remote/dist/server'
+    ),
+    path.resolve(
+        import.meta.dirname,
+        './examples/ssr-rspack-vue2_remote/node_modules/ssr-rspack-vue2_remote'
+    )
 );
