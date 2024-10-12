44518f82154a197e967aed8c8cd26153c1dc33a4
lzxb <1340641314@qq.com>
2024-10-12 15:16:26 +0800

    test: 添加测试

--- examples/ssr-rspack-vue2-remote/src/entry-server.ts
+++ examples/ssr-rspack-vue2-remote/src/entry-server.ts
@@ -1,5 +1,9 @@
 import type { ServerContext } from '@gez/core';
+import { log } from 'ssr-rspack-vue2-remote/src/utils/index';
+import Vue from 'vue';
 
 export default (ctx: ServerContext) => {
     ctx.html = '231323';
+    console.log('>>>>>>.', Vue.version);
+    console.log('log', log);
 };
