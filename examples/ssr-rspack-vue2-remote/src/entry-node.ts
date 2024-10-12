0d402ab263c62e0d0aad1578e3ce8b6228baa7e6
shizhongyuan1994 <849068437@qq.com>
2024-10-12 14:58:44 +0800

    feat: update module config

--- examples/ssr-rspack-vue2-remote/src/entry-node.ts
+++ examples/ssr-rspack-vue2-remote/src/entry-node.ts
@@ -22,6 +22,6 @@ export default defineNode({
         /**
          * 导出的文件
          */
-        exports: ['src:utils/index.ts', 'npm:vue']
+        exports: ['root:src/utils/index.ts', 'npm:vue']
     }
 });
