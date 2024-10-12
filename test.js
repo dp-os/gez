9aa82aa73ad5cc4649c34064222a17b6b11bcbe3
shizhongyuan1994 <849068437@qq.com>
2024-10-12 09:46:59 +0800

    feat: update symlink function

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-fs.symlinkSync(
-    path.resolve(
-        import.meta.dirname,
-        './examples/ssr-rspack-vue2_remote/dist/server'
-    ),
-    path.resolve(
-        import.meta.dirname,
-        './examples/ssr-rspack-vue2_remote/node_modules/ssr-rspack-vue2_remote'
-    )
+const targetDir = path.resolve(
+    import.meta.dirname,
+    './examples/ssr-rspack-vue2_remote/dist/server'
 );
+
+const linkDir = path.resolve(
+    import.meta.dirname,
+    './examples/ssr-rspack-vue2_remote/node_modules/ssr-rspack-vue2_remote'
+);
+
+if (fs.existsSync(linkDir)) {
+    fs.unlinkSync(linkDir);
+}
+
+fs.symlinkSync(targetDir, linkDir, 'dir');
