ff39936c9ae907e30b8ca8b299f97f15f4dfd48c
lzxb <1340641314@qq.com>
2024-11-01 11:27:56 +0800

    docs: 添加一个LOGO

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -3,15 +3,13 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
     root: path.join(__dirname, 'docs'),
+    globalStyles: path.join(__dirname, 'styles/index.css'),
     title: 'Gez',
     description:
         'Gez 是一个基于 Rspack 构建的 Pure ESM Like 解决方案，通过 importmap 将多服务的模块链接到具有哈希缓存的文件中。',
     icon: '/rspress-icon.png',
     base: '/gez/',
-    logo: {
-        light: '/rspress-light-logo.png',
-        dark: '/rspress-dark-logo.png'
-    },
+    logo: '/logo.svg',
     themeConfig: {
         lastUpdated: true,
         socialLinks: [
