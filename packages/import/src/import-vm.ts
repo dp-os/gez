5cb36752d953b4b0d15968cd208cea9cc1ac955f
lzxb <1340641314@qq.com>
2024-12-02 15:55:59 +0800

    feat: vm import 在循环依赖时,抛出错误

--- packages/import/src/import-vm.ts
+++ packages/import/src/import-vm.ts
@@ -5,38 +5,34 @@ import vm from 'node:vm';
 import IM from '@import-maps/resolve';
 import type { ImportMap } from './types';
 
+async function importBuiltinModule(specifier: string, context: vm.Context) {
+    const nodeModule = await import(specifier);
+    const keys = Object.keys(nodeModule);
+    const module = new vm.SyntheticModule(
+        keys,
+        function evaluateCallback() {
+            keys.forEach((key) => {
+                this.setExport(key, nodeModule[key]);
+            });
+        },
+        {
+            identifier: specifier,
+            context: context
+        }
+    );
+    await module.link(() => {
+        throw new TypeError(`Native modules should not be linked`);
+    });
+    await module.evaluate();
+    return module;
+}
+
 /**
  * 创建一个使用 vm 实现的 importmap 的 import 函数，可以创建多次来实现热更新效果，适合开发使用。
  */
 export function createVmImport(baseURL: URL, importMap: ImportMap = {}) {
     const parsedImportMap = IM.parse(importMap, baseURL);
-    async function moduleLinker(
-        specifier: string,
-        parent: string,
-        context: vm.Context,
-        cache = new Map<string, Promise<vm.SourceTextModule>>()
-    ) {
-        if (isBuiltin(specifier)) {
-            const nodeModule = await import(specifier);
-            const keys = Object.keys(nodeModule);
-            const module = new vm.SyntheticModule(
-                keys,
-                function evaluateCallback() {
-                    keys.forEach((key) => {
-                        this.setExport(key, nodeModule[key]);
-                    });
-                },
-                {
-                    identifier: specifier,
-                    context: context
-                }
-            );
-            await module.link(() => {
-                throw new TypeError(`Native modules should not be linked`);
-            });
-            await module.evaluate();
-            return module;
-        }
+    const parse = (specifier: string, parent: string) => {
         const result = IM.resolve(specifier, parsedImportMap, new URL(parent));
 
         let filename: string;
@@ -46,25 +42,57 @@ export function createVmImport(baseURL: URL, importMap: ImportMap = {}) {
             filename = import.meta.resolve(specifier, parent);
         }
         const url = new URL(filename);
-        const readFilename = url.pathname;
-        let module = cache.get(readFilename);
+        return {
+            filename,
+            url,
+            pathname: url.pathname
+        };
+    };
+    async function moduleLinker(
+        specifier: string,
+        parent: string,
+        context: vm.Context,
+        cache: Map<string, Promise<vm.SourceTextModule>>,
+        moduleIds: string[]
+    ) {
+        if (isBuiltin(specifier)) {
+            return importBuiltinModule(specifier, context);
+        }
+        const parsed = parse(specifier, parent);
+
+        if (moduleIds.includes(parsed.pathname)) {
+            throw new RangeError(
+                `Module circular reference: \n ${JSON.stringify([...moduleIds, parsed.pathname], null, 4)}`
+            );
+        }
+
+        const module = cache.get(parsed.pathname);
         if (module) {
             return module;
         }
-        const dirname = path.dirname(filename);
-        const build = async (): Promise<vm.SourceTextModule> => {
-            const text = fs.readFileSync(readFilename, 'utf-8');
+        const pe = new Promise<vm.SourceTextModule>((resolve) => {
+            process.nextTick(() => {
+                moduleBuild().then(resolve);
+            });
+        });
+
+        const dirname = path.dirname(parsed.filename);
+        cache.set(parsed.pathname, pe);
+        return pe;
+
+        async function moduleBuild(): Promise<vm.SourceTextModule> {
+            const text = fs.readFileSync(parsed.pathname, 'utf-8');
             const module = new vm.SourceTextModule(text, {
                 initializeImportMeta: (meta) => {
-                    meta.filename = filename;
+                    meta.filename = parsed.filename;
                     meta.dirname = dirname;
                     meta.resolve = (
                         specifier: string,
-                        parent: string | URL = url
+                        parent: string | URL = parsed.url
                     ) => {
                         return import.meta.resolve(specifier, parent);
                     };
-                    meta.url = url.toString();
+                    meta.url = parsed.url.toString();
                 },
                 identifier: specifier,
                 context: context,
@@ -72,27 +100,26 @@ export function createVmImport(baseURL: URL, importMap: ImportMap = {}) {
                 importModuleDynamically: (specifier, referrer) => {
                     return moduleLinker(
                         specifier,
-                        filename,
+                        parsed.filename,
                         // @ts-ignore
                         referrer.context,
-                        cache
+                        cache,
+                        [...moduleIds, parsed.pathname]
                     );
                 }
             });
             await module.link((specifier: string, referrer) => {
                 return moduleLinker(
                     specifier,
-                    filename,
+                    parsed.filename,
                     referrer.context,
-                    cache
+                    cache,
+                    [...moduleIds, parsed.pathname]
                 );
             });
             await module.evaluate();
             return module;
-        };
-        module = build();
-        cache.set(readFilename, module);
-        return module;
+        }
     }
     return async (
         specifier: string,
@@ -101,7 +128,13 @@ export function createVmImport(baseURL: URL, importMap: ImportMap = {}) {
         options?: vm.CreateContextOptions
     ) => {
         const context = vm.createContext(sandbox, options);
-        const module = await moduleLinker(specifier, parent, context);
+        const module = await moduleLinker(
+            specifier,
+            parent,
+            context,
+            new Map(),
+            []
+        );
 
         return module.namespace as Record<string, any>;
     };
