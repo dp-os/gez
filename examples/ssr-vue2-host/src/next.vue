18202a5e0d78c22b31cfd81c1a934af52f0dda23
lzxb <1340641314@qq.com>
2024-12-13 20:32:06 +0800

    example: ssr-vue2-host 添加点击交互

--- examples/ssr-vue2-host/src/next.vue
+++ examples/ssr-vue2-host/src/next.vue
@@ -1,6 +1,13 @@
 <template>
-    <button>这是自己服务的组件</button>
+    <div>
+        <p>Count: {{ count }}</p>
+        <button @click="count++">这是自己服务的组件，点击我试试。</button>
+    </div>
 </template>
+<script setup>
+import { ref } from 'vue'
+const count = ref(0);
+</script>
 <style lang="less" scoped>
 button {
     padding: 5px 10px;
