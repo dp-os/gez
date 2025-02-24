3fbdd3e9e30eeaba1cd0d403d2c990bd3996fcb5
lzxb <1340641314@qq.com>
2025-02-24 10:08:41 +0800

    feat: 支持 vue3 对模块进行依赖收集

--- packages/rspack-vue/src/vue3-loader.ts
+++ packages/rspack-vue/src/vue3-loader.ts
@@ -1,8 +1,23 @@
 import type { rspack } from '@gez/rspack';
 
+const ADD_IMPORT = `
+import { useSSRContext } from 'vue';
+function initImport () {
+
+    const mixins = Array.isArray(__exports__.mixins) ? __exports__.mixins : [];
+    mixins.push({
+        created () {
+            const ctx = useSSRContext();
+            ctx?.importMetaSet?.add(import.meta);
+        }
+    });
+    __exports__.mixins = mixins;
+}
+initImport();
+`;
+
 export default function (this: rspack.LoaderContext, text: string) {
-    // TODO
-    return text;
+    return text + ADD_IMPORT;
 }
 
 export const vue3Loader = new URL(import.meta.resolve(import.meta.url))
