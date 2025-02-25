eea029db1e393f856f29a2982e912ebe1d7c21f3
lzxb <1340641314@qq.com>
2025-02-25 17:14:19 +0800

    docs: 更新文档

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,12 @@ for src_dir in "$src_base"/ssr-*/dist/client "$src_base"/docs/dist/client; do
   fi  
 done
 
+# 复制 sitemap.xml 文件
+if [ -f "$src_base/docs/doc_build/sitemap.xml" ]; then
+  cp "$src_base/docs/doc_build/sitemap.xml" "$target_base/sitemap.xml"
+  echo "Copied sitemap.xml to $target_base"
+fi
+
 cd dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
